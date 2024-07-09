dec32fa9f9d381b29f5273b2bb2da20fd823a700
pravin006 <pravin201197@gmail.com>
2024-07-09 18:39:55 +0800

    create express server. set server to use /graphql endpoint

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import {graphqlHTTP} from 'express-graphql'
+import schema from './schema/schema.js'
+
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/graphql',graphqlHTTP({
+    schema:schema,
+    graphiql:process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`listening on ${port}`))
