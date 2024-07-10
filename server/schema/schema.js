54d01c9f286641c2c273715e480ab640d458d03f
pravin006 <pravin201197@gmail.com>
2024-07-10 20:55:58 +0800

    create graphql mutations to create,delete,update decks and cards

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,4 @@
-import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLBoolean, GraphQLSchema, GraphQLList } from 'graphql'
+import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLBoolean, GraphQLSchema, GraphQLList, GraphQLNonNull } from 'graphql'
 import { GraphQLDate } from 'graphql-scalars';
 // import { decks, cards } from '../sampleData.js'
 
@@ -8,7 +8,7 @@ import Card from '../models/Card.js'
 const DeckType = new GraphQLObjectType({
     name: 'Deck',
     fields: () => ({
-        deckId: {type: GraphQLID},
+        _id: {type: GraphQLID},
         deckName: {type: GraphQLString},
         isActive: {type: GraphQLBoolean},
         lastActive: {type: GraphQLDate},
@@ -24,7 +24,7 @@ const DeckType = new GraphQLObjectType({
 const CardType = new GraphQLObjectType({
     name: 'Card',
     fields: () => ({
-        cardId: {type: GraphQLID},
+        _id: {type: GraphQLID},
         deckId: {type: GraphQLID},
         question: {type: GraphQLString},
         answer: {type: GraphQLString},
@@ -43,9 +43,9 @@ const RootQuery = new GraphQLObjectType({
         },
         deck:{
             type: DeckType,
-            args: { deckId: {type: GraphQLID} },
+            args: { _id: {type: GraphQLID} },
             resolve(parent,args){
-                return Deck.findById(args.deckId)
+                return Deck.findById(args._id)
             }
         },
         
@@ -57,14 +57,112 @@ const RootQuery = new GraphQLObjectType({
         },
         card:{
             type: CardType,
-            args: { cardId: {type: GraphQLID} },
+            args: { _id: {type: GraphQLID} },
             resolve(parent,args){
-                return Card.findById(args.cardId)
+                return Card.findById(args._id)
+            }
+        }
+    }
+})
+
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addDeck: {
+            type: DeckType,
+            args: {
+                deckName: {type: GraphQLNonNull(GraphQLString)},
+                isActive: {type: GraphQLBoolean},
+                lastActive: {type: GraphQLDate}
+            },
+            resolve(parent,args){
+                const deck = new Deck({
+                    deckName: args.deckName,
+                    isActive: args.isActive || true,
+                    lastActive: args.lastActive || Date.now()
+                })
+                return deck.save()
+            }
+        },
+
+        deleteDeck:{
+            type: DeckType,
+            args: { _id: {type: GraphQLNonNull(GraphQLID)}},
+            resolve(parent,args){
+                return Deck.findOneAndDelete(args._id)
+            }
+        },
+
+        updateDeck:{
+            type:DeckType,
+            args:{
+                _id: {type: GraphQLNonNull(GraphQLID)},
+                deckName: {type: GraphQLString},
+                isActive: {type: GraphQLBoolean},
+                lastActive: {type: GraphQLDate},
+            },
+            resolve(parent,args){
+                return Deck.findByIdAndUpdate(args._id, {
+                    $set: {
+                        deckName: args.deckName,
+                        isActive: args.isActive,
+                        lastActive: args.lastActive,
+                    }},
+                    { new: true }
+                )
+            }
+        },
+
+        addCard:{
+            type: CardType,
+            args: {
+                deckId: {type: GraphQLNonNull(GraphQLID)},
+                question: {type: GraphQLNonNull(GraphQLString)},
+                answer: {type: GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parent,args){
+                const card = new Card({
+                    deckId: args.deckId,
+                    question: args.question,
+                    answer: args.answer,
+                    dueForReview: Date.now()
+                })
+                return card.save()
+            }
+        },
+
+        deleteCard:{
+            type: CardType,
+            args: {_id:{type: GraphQLNonNull(GraphQLID)}},
+            resolve(parent, args){
+                return Card.findOneAndDelete(args._id)
+            }
+        },
+
+        updateCard:{
+            type: CardType,
+            args: {
+                _id: {type: GraphQLNonNull(GraphQLID)},
+                question: {type: GraphQLString},
+                answer: {type: GraphQLString},
+                dueForReview: {type: GraphQLDate}
+            },
+            resolve(parent, args){
+                return Card.findByIdAndUpdate(args._id, {
+                    $set: {
+                        question: args.question,
+                        answer: args.answer,
+                        dueForReview: args.dueForReview
+                    }},
+                    { new: true }
+                )
             }
         }
     }
 })
 
 export default new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery,
+    mutation: mutation
 })
