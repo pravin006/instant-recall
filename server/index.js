fd3ca7554eae90d85917803ccc99291406d63670
pravin006 <pravin201197@gmail.com>
2024-07-10 18:47:04 +0800

    create connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 import express from 'express'
+import colors from 'colors'
 import dotenv from 'dotenv'
 import {graphqlHTTP} from 'express-graphql'
 import schema from './schema/schema.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
 
 const port = process.env.PORT || 5000
 
 const app = express()
+
+// Connect to DB
+connectDB()
 
 app.use('/graphql',graphqlHTTP({
     schema:schema,
