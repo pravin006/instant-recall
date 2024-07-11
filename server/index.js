011285a457517289feeee65870d10b9cc4f77a23
pravin006 <pravin201197@gmail.com>
2024-07-11 21:00:34 +0800

    use cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import colors from 'colors'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import {graphqlHTTP} from 'express-graphql'
 import schema from './schema/schema.js'
@@ -13,6 +14,8 @@ const app = express()
 
 // Connect to DB
 connectDB()
+
+app.use(cors())
 
 app.use('/graphql',graphqlHTTP({
     schema:schema,
