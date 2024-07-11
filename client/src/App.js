a64818a7ce59340849d8b77c4794e821b5217230
pravin006 <pravin201197@gmail.com>
2024-07-11 21:02:20 +0800

    import and use apollo client and provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import Header from './components/Header'
+import DecksPage from './components/DecksPage'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
     <>
-      <Header/>
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header/>
+        <div className="container">
+          <DecksPage/>
+        </div>
+      </ApolloProvider>
     </>
   );
 }
