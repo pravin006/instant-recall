b4465de7ab0523a589f5bdb4a99dd193bada3998
pravin006 <pravin201197@gmail.com>
2024-07-12 21:08:46 +0800

    create 404 page

---
+++ client/src/components/NotFoundPage.js
@@ -0,0 +1,16 @@
+import { FaExclamationTriangle } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+function NotFoundPage() {
+  return (
+    <div className='d-flex flex-column justify-content-center align-items-center mt-5'>
+        <FaExclamationTriangle className='text-danger' size='5em'/>
+        <h1>404</h1>
+        <h4>Page Not Found</h4>
+        <Link to='/' className='btn btn-secondary'>Go Back</Link>
+    </div>
+    
+  )
+}
+
+export default NotFoundPage
