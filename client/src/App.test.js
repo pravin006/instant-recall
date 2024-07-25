c4198f6b9bf31f2f834a307070a9eb38c5c73c21
pravin006 <pravin201197@gmail.com>
2024-07-25 18:48:09 +0800

    fix import

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen, act } from '@testing-library/react';
+import { act } from 'react'
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
