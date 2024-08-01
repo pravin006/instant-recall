bec466052bd8f34f77a79ccd63b2895b6ef6f736
pravin006 <pravin201197@gmail.com>
2024-08-01 20:24:31 +0800

    card styling

--- client/src/components/CardCard.js
+++ client/src/components/CardCard.js
@@ -11,7 +11,7 @@ function CardCard({card, color}) {
     <>
     <Card onClick={() => setModalShow(true)} bg='#49464e' text='white' style={{ cursor: 'pointer', backgroundColor:'#49464e', borderColor: color, borderWidth: '2px', height: '10rem' }}>
         <Card.Header style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', marginLeft: '1rem', marginRight: '1rem' }}>{card.question}</Card.Header>
-        <Card.Body  style={{ marginLeft: '1rem', marginRight: '1rem'}}>
+        <Card.Body  style={{  overflow: 'hidden',marginLeft: '1rem', marginRight: '1rem'}}>
             <Card.Text style={{ overflow: 'hidden',display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 4}}>{card.answer}</Card.Text>
         </Card.Body>
     </Card>
